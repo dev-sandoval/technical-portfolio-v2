2bf1ca3673c94348e65d8ce342aa5b13d4f7942b
Juan David Sandoval Salvador <102970701+sandovaldavid@users.noreply.github.com>
2025-10-31 18:18:17 -0500

    ♻️ refactor(shared-ui): use named exports in index.ts for LinkButton, LinkInline, SectionContainer and TitleSection

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,7 +1,7 @@
 export { Avatar } from './avatar';
 export { Badge } from './badge';
-export { default as LinkButton } from './link-button.astro';
-export { default as LinkInline } from './link-inline.astro';
-export { default as SectionContainer } from './section-container.astro';
+export { LinkButton } from './link-button';
+export { LinkInline } from './link-inline';
+export { SectionContainer } from './section-container';
 export { SocialPill } from './social-pill';
-export { default as TitleSection } from './title-section.astro';
+export { TitleSection } from './title-section';
