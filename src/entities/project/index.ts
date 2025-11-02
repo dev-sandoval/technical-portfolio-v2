3db3ddf1c5c380ec77b047d907b66e4a751f5bfe
Juan David Sandoval Salvador <102970701+sandovaldavid@users.noreply.github.com>
2025-11-01 21:28:14 -0500

    ✨ add(entities/project): extract ProjectCard UI component and export; update Projects widget to use it

--- src/entities/project/index.ts
+++ src/entities/project/index.ts
@@ -1,2 +1,3 @@
 export { getProjectsData } from './model';
 export type { ProjectItem } from './model';
+export { ProjectCard } from './ui';
---
+++ src/entities/project/ui/index.ts
@@ -0,0 +1,3 @@
+import ProjectCard from './ProjectCard.astro';
+
+export { ProjectCard };
